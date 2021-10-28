9da538fe9208cedf457e1a22da002085333b6758
MorsalinKausar043 <morsalinkausar043@gmail.com>
2021-10-29 00:20:34 +0600

    first client side done

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Routes from './routes/Routes';
+import "./App.css";
+
+const App = () => {
+  return (
+    <>
+      <Routes/>
+    </>
+  );
+};
+
+export default App;
---
+++ src/components/Home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <>
+            <h1>hello world this is react first page!</h1>
+        </>
+    );
+};
+
+export default Home;
